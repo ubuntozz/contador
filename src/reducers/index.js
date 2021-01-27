5b48bcb495c18c87ed0a8d41428472a2f31e05a3
ubuntozz <63567435+ubuntozz@users.noreply.github.com>
2021-01-26 19:52:42 -0600

    initial commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
-export default (state = 0, action) => {
+export default (state = 10, action) => {
   switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
     default:
-      return state
+      return state;
   }
-}
+};
